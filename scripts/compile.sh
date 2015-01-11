31a6bb9913547271bf93f0446eb115eedd387504
Kevin Altman <kevin@itsthatguy.com>
2015-01-11 16:16:45 -0500

    removing javascript source assets from the rails-gem

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,4 +3,4 @@ echo 'Creating lib folder...'
 mkdir -pv lib/assets/javascripts lib/assets/stylesheets && echo 'Done!\n'
 
 echo 'Copying assets into the lib folder...'
-mv -fv node_modules/peteshow/lib/assets/* lib/assetsi && echo 'Done!\n'
+mv -fv node_modules/peteshow/lib/assets/* lib/assets && echo 'Done!\n'
